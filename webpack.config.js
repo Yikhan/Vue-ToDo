b8cdf83de8580199103c26e3d7f9d0f9bfd4a7a1
Yikhan <yhxiaoch@gmail.com>
2019-01-13 23:22:09 +1100

    basic config using webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: '[name]-trans.[ext]'
+                        }
+                    }
+                ]
+            }
+            
+        ]
+    },
+    mode: 'none',
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
