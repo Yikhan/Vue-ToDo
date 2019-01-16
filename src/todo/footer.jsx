ca6815c17deab6e860d87cce8ed2cccab4e26696
Yikhan <yhxiaoch@gmail.com>
2019-01-17 00:15:35 +1100

    add footer and styles

---
+++ src/todo/footer.jsx
@@ -0,0 +1,18 @@
+import '../assets/styles/footer.styl'
+
+export default {
+  data() {
+    return (
+      {
+        author: 'Yihan'
+      }
+    )
+  },
+  render() {
+    return (
+      <div id="footer">
+        <span>Written by {this.author}</span>
+      </div>
+    )
+  }
+}
